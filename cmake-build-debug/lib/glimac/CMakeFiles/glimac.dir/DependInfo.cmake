e49ceb6f5556ff3aac2d0d4d497e0206fad3373a
Laurine Capdeville <capdevillelaurine2@gmail.com>
2020-12-08 16:34:55 +0100

    Initialisation du projet

---
+++ cmake-build-debug/lib/glimac/CMakeFiles/glimac.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/lib/glimac/src/Cone.cpp" "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/cmake-build-debug/lib/glimac/CMakeFiles/glimac.dir/src/Cone.cpp.o"
+  "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/lib/glimac/src/FreeflyCamera.cpp" "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/cmake-build-debug/lib/glimac/CMakeFiles/glimac.dir/src/FreeflyCamera.cpp.o"
+  "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/lib/glimac/src/Geometry.cpp" "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/cmake-build-debug/lib/glimac/CMakeFiles/glimac.dir/src/Geometry.cpp.o"
+  "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/lib/glimac/src/Image.cpp" "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/cmake-build-debug/lib/glimac/CMakeFiles/glimac.dir/src/Image.cpp.o"
+  "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/lib/glimac/src/Program.cpp" "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/cmake-build-debug/lib/glimac/CMakeFiles/glimac.dir/src/Program.cpp.o"
+  "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/lib/glimac/src/SDLWindowManager.cpp" "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/cmake-build-debug/lib/glimac/CMakeFiles/glimac.dir/src/SDLWindowManager.cpp.o"
+  "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/lib/glimac/src/Shader.cpp" "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/cmake-build-debug/lib/glimac/CMakeFiles/glimac.dir/src/Shader.cpp.o"
+  "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/lib/glimac/src/Sphere.cpp" "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/cmake-build-debug/lib/glimac/CMakeFiles/glimac.dir/src/Sphere.cpp.o"
+  "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/lib/glimac/src/TrackballCamera.cpp" "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/cmake-build-debug/lib/glimac/CMakeFiles/glimac.dir/src/TrackballCamera.cpp.o"
+  "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/lib/glimac/src/tiny_obj_loader.cpp" "/home/laurine/Documents/IMAC2/OpenGL/Opengl_Projet_Capdeville_ChamotRooke_Rostoker/cmake-build-debug/lib/glimac/CMakeFiles/glimac.dir/src/tiny_obj_loader.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/glimac/include"
+  "../lib/glm/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
