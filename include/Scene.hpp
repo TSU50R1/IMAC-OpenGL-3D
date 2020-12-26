8b2e1f19b074adbdf59a43ca40bf7776ed3243d5
Laurine Capdeville <capdevillelaurine2@gmail.com>
2020-12-26 17:23:46 +0100

    classe scene

---
+++ include/Scene.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "myShader.hpp"
+#include "Model.hpp"
+#include "LoadINI.hpp"
+#include <string>
+#include <vector>
+
+class Scene {
+public:
+    size_t m_nbModels;
+    size_t m_nbShaders;
+    IniLoadMap m_IniFile;
+    std::vector<Model*> m_models;
+    std::vector<Shader*> m_shaders;
+
+public:
+    Scene(const std::string filename);
+    ~Scene();
+
+    void loadScene();
+    void renderScene(glm::mat4 &projection, glm::mat4 &view);
+};
