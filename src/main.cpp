568e0bb27600961f6cb28532ccce71dc05273c18
Daphné Chamot-Rooke <dchamotr@etud.u-pem.fr>
2020-12-28 22:51:43 +0100

    event

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "Model.hpp"
 #include "LoadINI.hpp"
 #include "Scene.hpp"
+#include "Event.hpp"
 
 #include <iostream>
 #include <string>
@@ -18,7 +19,7 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-void processInput(GLFWwindow *window);
+void processInput(GLFWwindow *window, float t);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
@@ -73,117 +74,6 @@ int main(){
         std::cerr << glewGetErrorString(glewInitError) << std::endl;
         return EXIT_FAILURE;
     }
-	
-
-    // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
-    stbi_set_flip_vertically_on_load(true);
-
-    // configure global opengl state
-    // -----------------------------
-    glEnable(GL_DEPTH_TEST);
-
-    // build and compile shaders
-    // -------------------------
-    Shader ourShader("/mnt/e/Bibliothèques/Documents/IMAC/INFO/Imacity/IMAC-OpenGL-3D/src/shaders/3D.vs.glsl",
-         "/mnt/e/Bibliothèques/Documents/IMAC/INFO/Imacity/IMAC-OpenGL-3D/src/shaders/lightning.fs.glsl");
-
-    // load models
-    // -----------
-    //Model ourModel("/mnt/e/Bibliothèques/Documents/IMAC/INFO/Imacity/IMAC-OpenGL-3D/assets/models/backpack.obj");
-
-    // draw in wireframe
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-    // render loop
-    // -----------
-    while (!glfwWindowShouldClose(window)){
-        // per-frame time logic
-        // --------------------
-        float currentFrame = glfwGetTime();
-        deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
-
-        // input
-        // -----
-        processInput(window);
-
-        // render
-        // ------
-        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // don't forget to enable shader before setting uniforms
-        ourShader.use();
-
-        //uniforms
-        GLint uMVPMatrix = glGetUniformLocation(ourShader.ID, "uMVPMatrix");
-        GLint uMVMatrix = glGetUniformLocation(ourShader.ID, "uMVMatrix");
-        GLint uNormalMatrix = glGetUniformLocation(ourShader.ID, "uNormalMatrix");
-        GLint uLightIntensity = glGetUniformLocation(ourShader.ID, "uLightIntensity");
-        GLint uLightDir_vs = glGetUniformLocation(ourShader.ID, "uLightDir_vs");
-        GLint uKd = glGetUniformLocation(ourShader.ID, "uKd");
-        GLint uKs = glGetUniformLocation(ourShader.ID, "uKs");
-        GLint uShininess = glGetUniformLocation(ourShader.ID, "uShininess");
-
-        // view/projection transformations
-        glm::mat4 projection = glm::perspective(glm::radians(70.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        glm::mat4 view = camera.getViewMatrix();
-        ourShader.setMat4("projection", projection);
-        ourShader.setMat4("view", view);
-
-        glm::mat4 lightMVMatrix = glm::rotate(projection, 4.75f, glm::vec3(-1, -1.5, 0)); // Translation * Rotation
-        glm::vec3 lightDir_vs(lightMVMatrix * glm::vec4(1, 1, 1, 0));
-        glUniform3f(uLightIntensity, 1, .5f, .5f);
-        glUniform3fv(uLightDir_vs, 1, glm::value_ptr(lightDir_vs));
-        glUniform3f(uKd, 0.05, 0, 1);
-        glUniform3f(uKs, 0.15, 0.15, 1);
-        glUniform1f(uShininess, 2);
-
-
-        // render the loaded model
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f, 0.0f, -40.0f)); // translate it down so it's at the center of the scene
-        model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));	// it's a bit too big for our scene, so scale it down
-        ourShader.setMat4("model", model);
-        //ourModel.Draw(ourShader);
-
-
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        //-------------------------------------------------------------------------------
-		
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
-    glfwTerminate();
-    return 0;
-}
-
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window){
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-
-    //if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-    //    camera.ProcessKeyboard(FORWARD, deltaTime);
-    //if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-    //    camera.ProcessKeyboard(BACKWARD, deltaTime);
-    //if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-    //    camera.ProcessKeyboard(LEFT, deltaTime);
-    //if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-    //    camera.ProcessKeyboard(RIGHT, deltaTime);
-}
-
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height){
-    // make sure the viewport matches the new window dimensions; note that width and
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
-}
 
     // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
     stbi_set_flip_vertically_on_load(true);
@@ -210,6 +100,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height){
 
     // render loop
     // -----------
+    static const float speed = 0.2;
     while (!glfwWindowShouldClose(window)){
         // per-frame time logic
         // --------------------
@@ -219,7 +110,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height){
 
         // input
         // -----
-        processInput(window);
+        Event event (camera);
+        event.processInput(window, speed);
 
         // render
         // ------
@@ -227,6 +119,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height){
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // don't forget to enable shader before setting uniforms
+
 
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(70.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
@@ -249,19 +142,50 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height){
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window){
+/*void processInput(GLFWwindow *window, float t){
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
-
-    //if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-    //    camera.ProcessKeyboard(FORWARD, deltaTime);
-    //if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-    //    camera.ProcessKeyboard(BACKWARD, deltaTime);
-    //if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-    //    camera.ProcessKeyboard(LEFT, deltaTime);
-    //if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-    //    camera.ProcessKeyboard(RIGHT, deltaTime);
-}
+    if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS)
+    {
+      // call camera to move FORWARD
+      camera.moveFront(t);
+    }
+    if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS)
+    {
+      // call camera to move BACKWARD
+      camera.moveFront(-t);
+    }
+    if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS)
+    {
+      // call camera to move LEFT
+      camera.moveLeft(t/60);
+    }
+    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
+    {
+      // call camera to move RIGHT
+      camera.moveLeft(-t/60);
+    }
+    if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS)
+    {
+      // call camera to ROTATE LEFT
+      camera.rotateLeft(t);
+    }
+    if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS)
+    {
+      // call camera to ROTATE RIGHT
+      camera.rotateLeft(-t);
+    }
+    if (glfwGetKey(window, GLFW_KEY_Y) == GLFW_PRESS)
+    {
+      // call camera to move ROTATE UP
+      camera.rotateUp(t);
+    }
+    if (glfwGetKey(window, GLFW_KEY_H) == GLFW_PRESS)
+    {
+      // call camera to move ROTATE DOWN
+      camera.rotateUp(-t);
+    }
+}*/
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
