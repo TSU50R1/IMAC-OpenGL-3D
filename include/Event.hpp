44f86c1b37e2c8b48a5772f9a0c1701466e61daf
Daphné Chamot-Rooke <root@DAPH-TOSH.localdomain>
2020-12-28 22:31:51 +0100

    ajout de la classe event

--- include/Event.hpp
+++ include/Event.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "FreeflyCamera.hpp"
+
 class Event {
-
 public:
-
-private:
+  FreeflyCamera m_camera;
+public:
+    Event(FreeflyCamera _camera);
+    ~Event();
+    void processInput(GLFWwindow *window, float t);
 
 };
